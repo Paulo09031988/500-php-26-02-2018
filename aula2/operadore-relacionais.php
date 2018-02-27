926c9a15bfaf4d7b3c65477fd3959803f9f054cc
Paulo09031988 <zete.araujo@gmail.com>
2018-02-27 20:32:24 -0300

    teste2

---
+++ aula2/operadore-relacionais.php
@@ -0,0 +1,43 @@
+<?php
+
+echo "<pre>";
+
+// Igual
+var_dump(2 == 2);
+var_dump(2 == "2");
+var_dump(2 == 3);
+echo <"hr>";
+
+//Identico
+var_dump( 2 === 2);
+var_dump( 2 === "2");
+echo "<hr>";
+
+// Diferente 
+var_dump( 2 != 2);
+var_dump( 3 != 2);
+echo "<br>";
+
+// Maior que
+var_dump( 2 > 5);
+var_dump( 5 > 2);
+var_dump( 2 > 2);
+echo "<br>";
+
+// Menor que
+var_dump( 2 < 5);
+var_dump( 5 < 2);
+var_dump( 2 < 2);
+echo "<br>";
+
+// Menor ou Igual
+var_dump( 2 <= 5);
+var_dump( 5 <= 2):
+var_dump( 2 <= 2);
+echo "<br>"
+
+// Maior ou  igual
+var_dump( 2 >= 5);
+var_dump( 5 >= 2);
+var_dump( 2 >= 2);
+echo "<br>"
