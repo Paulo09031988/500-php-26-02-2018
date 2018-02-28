ee8d2848203efec7217d85b0c9bbcc75a736bdc0
Paulo09031988 <zete.araujo@gmail.com>
2018-02-28 20:05:53 -0300

    inicio aula3

---
+++ Aula3/for.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "<pre>";
+for ($cont = 0; $cont <= 10; $cont++){
+	echo $cont . "\n";
+}
+
+
+echo "<pre>";
+for ($cont = 10; $cont >= 0; $cont--){
+	echo $cont . "\n";
+}
---
+++ Aula3/while.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "<pre>";
+$cont = 0;
+
+while ($cont <= 10) {
+	echo $cont;
+	echo  " - 4linux \n";
+
+	if ($cont == 1){
+		echo "Legal";
+	}
+	$cont++;
+}
+
+echo "Acabou";
