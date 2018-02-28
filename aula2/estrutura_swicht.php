196ccfffe46e3fc5a6ad5d3988bb91743ea58592
Paulo09031988 <zete.araujo@gmail.com>
2018-02-27 22:20:24 -0300

    teste6

---
+++ aula2/estrutura_swicht.php
@@ -0,0 +1,16 @@
+<?php
+
+$nome = readline("Digite seu nome:");
+
+
+switch ($nome) {
+	case 'Lucas':
+		echo "Eae Lucas";
+		break;
+	case 'Diego':
+		echo "Eae Diego";
+		break;
+	case '':
+		echo "Eae Lucas";
+		break;
+}
