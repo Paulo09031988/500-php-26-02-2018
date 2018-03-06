6c9974aeda5bf1c3a847cc68bd00dfb497f6e8e8
Paulo09031988 <zete.araujo@gmail.com>
2018-03-05 22:15:56 -0300

    aula6

---
+++ calc/calc.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>Calculadora Básica</title>
+    </head>
+ 
+    <body>
+        <form action="calculadora.php" method="get">
+ 
+            Primeiro numero:
+            <input name="num1" type="text" />
+            <br /> <br />
+             
+            Segundo numero:
+            <input name="num2" type="text" />
+            <br /> <br />
+             
+            <input type="submit" value="submeter" />    
+            <br /> <br />
+             
+        </form>
+    </body>
+</html>
